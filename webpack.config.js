2dde8cf2dc4d241fe6f8a938561e8f9c12fccd63
Maxime Fabre <ehtnam6@gmail.com>
2015-10-09 03:22:14 +0200

    Common chunk plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,20 @@
+var webpack = require('webpack');
+
 module.exports = {
-    entry:  './src',
-    output: {
+    entry:   './src',
+    output:  {
         path:       'builds',
         filename:   'bundle.js',
         publicPath: 'builds/',
     },
-    module: {
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            name:      'main', // Move dependencies to our main file
+            children:  true, // Look for common dependencies in all children,
+            minChunks: 2, // How many times a dependency must come up before being extracted
+        }),
+    ],
+    module:  {
         loaders: [
             {
                 test:    /\.js/,
